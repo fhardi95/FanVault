fdc2733e50414a4478dbad69f529242f232a477c
Fhardi Said <fhardi.said@gmail.com>
2026-08-30 01:39:01 +0100

    Update existing files and add new files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 export const metadata = {
   title: "FanVault — Product Catalog",
+  other: {
+    "p:domain_verify": "12iD3XhCQfYF5sf6FaMrzrGFxzrKJ4u85L",
+  },
 };
 
 export default function RootLayout({
